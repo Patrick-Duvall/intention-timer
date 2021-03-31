521512cba4f7873bc15998bcf18d3ba2455ae9dc
Patrick Duvall <duvallp208@gmail.com>
2021-03-31 17:10:19 -0600

    Add color change functionality all category buttons

--- main.js
+++ main.js
@@ -1,12 +1,25 @@
 var studyButton = document.querySelector("#study-button")
-var studyImage = document.querySelector("#study-image")
-// var studyButton = document.querySelector("#study-button")
-// var studyButton = document.querySelector("#study-button")
+var meditateButton = document.querySelector("#meditate-button")
+var exerciseButton = document.querySelector("#exercise-button")
 
 studyButton.addEventListener("click", colorStudyButton)
+meditateButton.addEventListener("click", colorMeditateButton)
+exerciseButton.addEventListener("click", colorExerciseButton)
 
 function colorStudyButton() {
-  // event.preventDefault
-  // debugger
   studyButton.classList.add("green")
+  meditateButton.classList.remove("purple")
+  exerciseButton.classList.remove("red")
+}
+
+function colorMeditateButton() {
+  meditateButton.classList.add("purple")
+  exerciseButton.classList.remove("red")
+  studyButton.classList.remove("green")
+}
+
+function colorExerciseButton() {
+  exerciseButton.classList.add("red")
+  studyButton.classList.remove("green")
+  meditateButton.classList.remove("purple")
 }
