0ad0b5cf188233f1c64f70026e2c6d4394f5262c
Patrick Duvall <duvallp208@gmail.com>
2021-04-01 15:08:10 -0600

    Add custom validations that appear under form fields

--- main.js
+++ main.js
@@ -6,6 +6,40 @@ studyButton.addEventListener("click", colorStudyButton)
 meditateButton.addEventListener("click", colorMeditateButton)
 exerciseButton.addEventListener("click", colorExerciseButton)
 
+form = document.querySelector(".inner-activity-container")
+form.addEventListener("invalid", function (event) {
+  event.preventDefault();
+  }, true
+)
+
+
+var submitButton = form.querySelector("#start")
+submitButton.addEventListener("click", function () {
+  var invalidFields = form.querySelectorAll(":invalid")
+  var errorMessages = form.querySelectorAll(".error-message"),
+    parent;
+    debugger
+    
+    // Remove any existing messages
+    for (var i = 0; i < errorMessages.length; i++) {
+      errorMessages[i].parentNode.removeChild(errorMessages[i]);
+    }
+    debugger
+    // Add errors
+    for (var i = 0; i < invalidFields.length; i++) {
+      parent = invalidFields[i].parentNode;
+      invalidFields[i].validationMessage = invalidFields[i].oninvalid()
+      parent.insertAdjacentHTML("beforeend", "<div class='error-message'>" +
+      invalidFields[i].validationMessage +
+      "</div>");
+    }
+    debugger
+  // If there are errors, give focus to the first invalid field
+  if (invalidFields.length > 0) {
+    invalidFields[0].focus();
+  }
+});
+
 function colorStudyButton() {
   studyButton.classList.add("green")
   meditateButton.classList.remove("purple")
