cec4f4ef7748d35f83d4341495074125acaedbfc
Patrick Duvall <duvallp208@gmail.com>
2021-04-01 15:35:48 -0600

    Add coloring to error messages

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 var studyButton = document.querySelector("#study-button")
 var meditateButton = document.querySelector("#meditate-button")
 var exerciseButton = document.querySelector("#exercise-button")
+var mainPage = document.querySelector('.main-page')
 
-studyButton.addEventListener("click", colorStudyButton)
-meditateButton.addEventListener("click", colorMeditateButton)
-exerciseButton.addEventListener("click", colorExerciseButton)
+studyButton.addEventListener("click", setFormGreen)
+meditateButton.addEventListener("click", setFormPurple)
+exerciseButton.addEventListener("click", setFormRed)
 
 form = document.querySelector(".inner-activity-container")
 form.addEventListener("invalid", function (event) {
@@ -18,13 +19,10 @@ submitButton.addEventListener("click", function () {
   var invalidFields = form.querySelectorAll(":invalid")
   var errorMessages = form.querySelectorAll(".error-message"),
     parent;
-    debugger
-    
     // Remove any existing messages
     for (var i = 0; i < errorMessages.length; i++) {
       errorMessages[i].parentNode.removeChild(errorMessages[i]);
     }
-    debugger
     // Add errors
     for (var i = 0; i < invalidFields.length; i++) {
       parent = invalidFields[i].parentNode;
@@ -33,27 +31,26 @@ submitButton.addEventListener("click", function () {
       invalidFields[i].validationMessage +
       "</div>");
     }
-    debugger
   // If there are errors, give focus to the first invalid field
   if (invalidFields.length > 0) {
     invalidFields[0].focus();
   }
 });
 
-function colorStudyButton() {
-  studyButton.classList.add("green")
-  meditateButton.classList.remove("purple")
-  exerciseButton.classList.remove("red")
+function setFormGreen() {
+  mainPage.classList.add("green")
+  mainPage.classList.remove("purple")
+  mainPage.classList.remove("red")
 }
 
-function colorMeditateButton() {
-  meditateButton.classList.add("purple")
-  exerciseButton.classList.remove("red")
-  studyButton.classList.remove("green")
+function setFormPurple() {
+  mainPage.classList.add("purple")
+  mainPage.classList.remove("red")
+  mainPage.classList.remove("green")
 }
 
-function colorExerciseButton() {
-  exerciseButton.classList.add("red")
-  studyButton.classList.remove("green")
-  meditateButton.classList.remove("purple")
+function setFormRed() {
+  mainPage.classList.add("red")
+  mainPage.classList.remove("green")
+  mainPage.classList.remove("purple")
 }
